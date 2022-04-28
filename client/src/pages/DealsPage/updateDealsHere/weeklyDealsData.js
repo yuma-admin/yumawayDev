63bbcf16fb82c828509f6cfcee6ff4a8970bba8a
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-04-27 21:33:06 -0600

    weekly update

--- client/src/pages/DealsPage/updateDealsHere/weeklyDealsData.js
+++ client/src/pages/DealsPage/updateDealsHere/weeklyDealsData.js
@@ -26,18 +26,18 @@ const weeklyDealsData =
       {
         imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-8.png",
       },
-      {
-        imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-9.png",
-      },
-      {
-        imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-10.png",
-      },
-      {
-        imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-11.png",
-      },
-      {
-        imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-12.png",
-      },
+      // {
+      //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-9.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-10.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-11.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-12.png",
+      // },
       // {
       //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-13.png",
       // },
@@ -50,18 +50,18 @@ const weeklyDealsData =
     ],
 
     michigan: [
-      {
-        imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-1.png",
-      },
-      {
-        imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-2.png",
-      },
-      {
-        imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-3.png",
-      },
-      {
-        imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-4.png",
-      },
+      // {
+      //   imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-1.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-2.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-3.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-4.png",
+      // },
     //   {
     //     imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-5.png",
     //   },
