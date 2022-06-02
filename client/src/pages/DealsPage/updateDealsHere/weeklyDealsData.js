81837b697e7a0cf2599091f96fec48a3e2274b14
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-06-01 20:22:32 -0600

    weekly update

--- client/src/pages/DealsPage/updateDealsHere/weeklyDealsData.js
+++ client/src/pages/DealsPage/updateDealsHere/weeklyDealsData.js
@@ -20,12 +20,12 @@ const weeklyDealsData =
       {
         imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-6.png",
       },
-      {
-        imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-7.png",
-      },
-      {
-        imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-8.png",
-      },
+      // {
+      //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-7.png",
+      // },
+      // {
+      //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-8.png",
+      // },
       // {
       //   imgUrl: "/images/deals/coloradoDeals/weekly-update/coupon-9.png",
       // },
@@ -50,9 +50,9 @@ const weeklyDealsData =
     ],
 
     michigan: [
-      // {
-      //   imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-1.png",
-      // },
+      {
+        imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-1.png",
+      },
       // {
       //   imgUrl: "/images/deals/michiganDeals/weekly-update/coupon-2.png",
       // },
