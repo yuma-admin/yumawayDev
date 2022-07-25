cef4deb3a5336fbb8ea13d860ed0dbe8d2a1fb97
Danney Trieu <89677528+Danneytrieu@users.noreply.github.com>
2022-07-25 15:36:22 -0600

    fix weekly update

--- client/src/pages/DealsPage/DealsPage.js
+++ client/src/pages/DealsPage/DealsPage.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./newDealsCSS.css";
 
+// State Select Modal
+import StateSelectModal from "../../components/StateSelectModal/StateSelectModal";
+import { createPortal } from "react-dom";
+
 // Meta Data
 import { Helmet } from "react-helmet";
 
@@ -16,9 +20,9 @@ import dealsGridController from "./page-setup/logic/dealsGridBuilder";
 // PAGE START
 function CSSGrid() {
   // Jane's Tracking Script
-    const janescript = document.createElement("script");
-    janescript.async = true;
-    janescript.src = `document.addEventListener("click", function(e) {
+  const janescript = document.createElement("script");
+  janescript.async = true;
+  janescript.src = `document.addEventListener("click", function(e) {
             var linkNode = e.srcElement.href ? e.srcElement : e.srcElement.parentNode;
         if(linkNode.localName !== "a" || linkNode.href.endsWith("#")) return;
         
@@ -33,70 +37,86 @@ function CSSGrid() {
         location.assign(url.toString());
         }
         })`;
-    document.head.appendChild(janescript);
+  document.head.appendChild(janescript);
 
-    // Sets Data Based on State Selected and Logic Functions (Above)
-    const [stateSelected, setStateSelected] = useState("");
-    const [carouselDeals, setCarouselDeals] = useState([""]);
-    const [topRightCoupon, setTopRightCoupon] = useState("");
-    const [weeklyDeals, setWeeklyDeals] = useState([""]);
-    const [cssGridTemplate, setCssGridTemplate] = useState([""]);
+  // Sets Data Based on State Selected and Logic Functions (Above)
+  const [stateSelected, setStateSelected] = useState("");
+  const [carouselDeals, setCarouselDeals] = useState([""]);
+  const [topRightCoupon, setTopRightCoupon] = useState("");
+  const [weeklyDeals, setWeeklyDeals] = useState([""]);
+  const [cssGridTemplate, setCssGridTemplate] = useState([""]);
 
-    useEffect(() => {
-        // Determines Page Setup Based on State Location Selected
-        const chosenStateDealData = dealsDataController();
-        // Builds CSS Grid Based on Number of Weekly Coupons for that State
-        const chosenCssGrid = dealsGridController(
-        chosenStateDealData.chosenWeeklyDeals.length
-        );
-
-        // Sets All Data Based on Above Logic
-        setStateSelected(chosenStateDealData.stateData);
-        setCarouselDeals(chosenStateDealData.chosenCarousel);
-        setTopRightCoupon(chosenStateDealData.chosenTopRightCoupon);
-        setWeeklyDeals(chosenStateDealData.chosenWeeklyDeals);
-        setCssGridTemplate(chosenCssGrid);
-    }, []);
-
-    return (
-        <>
-        <Helmet>
-            <title>Yuma Way | Browse Our Current Deals</title>
-            <meta charset="utf-8" />
-            <meta
-            name="description"
-            content="Browse Our Current Deals for Both Colorado & Michigan Locations"
-            ></meta>
-            <link rel="canonical" href="http://yumaway.com/colorado-deals"></link>
-        </Helmet>
-        
-        <div className="dealContainer">
-            {/* STATE SELECTOR  (Dropdown Menu) */}
-            <div id="dealsStateSelectorSection">
-            <StateSelector stateSelectedData={stateSelected} />
-            </div>
-
-            {/* TOP OF PAGE (Main Carousel and Right Column) */}
-            <div id="dealsTopPageSection">
-            <TopPageDeals
-                carouselData={carouselDeals}
-                topRight={topRightCoupon}
-            />
-            </div>
-
-            {/* WEEKLY DEALS (UPDATED EVERY WEEK) */}
-            {/* SEE updateDealsHere folder for details */}
-            <div id="dealsWeeklyUpdate">
-            <CouponGrid
-                topRight={topRightCoupon}
-                carouselData={carouselDeals}
-                couponImages={weeklyDeals}
-                cssGridTemplate={cssGridTemplate}
-            />
-            </div>
-        </div>
-        </>
+  useEffect(() => {
+    // Determines Page Setup Based on State Location Selected
+    const chosenStateDealData = dealsDataController();
+    // Builds CSS Grid Based on Number of Weekly Coupons for that State
+    const chosenCssGrid = dealsGridController(
+      chosenStateDealData.chosenWeeklyDeals.length
     );
+
+    // Sets All Data Based on Above Logic
+    setStateSelected(chosenStateDealData.stateData);
+    setCarouselDeals(chosenStateDealData.chosenCarousel);
+    setTopRightCoupon(chosenStateDealData.chosenTopRightCoupon);
+    setWeeklyDeals(chosenStateDealData.chosenWeeklyDeals);
+    setCssGridTemplate(chosenCssGrid);
+  }, []);
+
+  ///////////////////////////////////////////////////////////
+  //  STATE SELECT MODAL
+  const [showStateModal, setStateModal] = useState([""]);
+  const Modal = ({ showStateModal, setStateModal }) => {
+    const content = showStateModal && <StateSelectModal  />;
+    return createPortal(content, document.body);
+  };
+  ///////////////////////////////////////////////////////////
+
+  return (
+    <>
+      <Helmet>
+        <title>Yuma Way | Browse Our Current Deals</title>
+        <meta charset="utf-8" />
+        <meta
+          name="description"
+          content="Browse Our Current Deals for Both Colorado & Michigan Locations"
+        ></meta>
+        <link rel="canonical" href="http://yumaway.com/colorado-deals"></link>
+      </Helmet>
+
+      <div className="dealContainer">
+        {/* STATE SELECTOR  (Dropdown Menu) */}
+        <div id="dealsStateSelectorSection">
+          <StateSelector stateSelectedData={stateSelected} />
+        </div>
+
+        {/* TOP OF PAGE (Main Carousel and Right Column) */}
+        <div id="dealsTopPageSection">
+          <TopPageDeals
+            carouselData={carouselDeals}
+            topRight={topRightCoupon}
+          />
+        </div>
+
+        {/* WEEKLY DEALS (UPDATED EVERY WEEK) */}
+        {/* SEE updateDealsHere folder for details */}
+        <div id="dealsWeeklyUpdate">
+          <CouponGrid
+            topRight={topRightCoupon}
+            carouselData={carouselDeals}
+            couponImages={weeklyDeals}
+            cssGridTemplate={cssGridTemplate}
+          />
+        </div>
+      </div>
+
+      {/* HIDDEN MODAL */}
+      {/* <Modal
+        showStateModal={showStateModal}
+        // data-keyboard="false" data-backdrop="static"
+      ></Modal> */}
+
+    </>
+  );
 }
 
 export default CSSGrid;
