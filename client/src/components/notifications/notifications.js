96be166375a515aa2800c2b10d517ebb727abf32
tomangotti <tom.angotti11@gmail.com>
2025-05-03 09:34:34 -0600

    updated ui for notification

--- client/src/components/notifications/notifications.js
+++ client/src/components/notifications/notifications.js
@@ -5,9 +5,9 @@ const Notifications = () => {
     return (
         <div style={{ 
             backgroundColor: "#d9ab73", padding: "0.5rem", textAlign: "center" }}>
-            <div style={{ margin: "0 auto", color: "rgb(24,24,24)", borderTop: "1px solid rgb(24,24,24)", borderBottom: "1px solid rgb(24,24,24)" , padding: "0px" }}>
-                {/* <p style={{fontSize: 20}}>ANNOUNCEMENT</p> */}
-                <p>ANNOUNCEMENT! We are REBRANDING to Gardens Dispensary and launching a BRAND NEW site in the near future! Please be on the look our for our changes in the coming days!</p>
+            <div style={{ 
+                justifyContent: "center", alignItems: "center", display: "flex", color: "rgb(24,24,24)", borderTop: "1px solid rgb(24,24,24)", borderBottom: "1px solid rgb(24,24,24)", height: "100%", padding: "0.5rem" }}>
+                <p style={{ margin: "auto" }}>ANNOUNCEMENT! We are REBRANDING to Gardens Dispensary and launching a BRAND NEW site in the near future! Please be on the look our for our changes in the coming days!</p>
             </div>
         </div>
     );
